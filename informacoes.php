62e6c95eb34f90166c1a925feb835ff497a481b0
eduardogonsalves <c.edugonsalves@gmail.com>
2024-06-14 08:35:50 -0300

    Página de informações úteis

---
+++ informacoes.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informações Úteis</title>
+</head>
+<body>
+    <h1>Informações Úteis</h1>
+    
+    <h2>Telefonia/Comunicação</h2>
+        <p>
+            Para ligações internacionais feitas para João Pessoa, discar 00 + 55 + 83 + número de telefone desejado.
+        </p>
+
+    <h2>Telefones Úteis</h2>
+        <p>
+            Bombeiros: 193
+            Ceatox: (83) 3224 6688
+            Defesa Civil: 199
+            Disque Denúncia (Exploração Infanto-Juvenil): 0800 2827969 ou 100
+            Polícia Civil: 197
+            Polícia Federal: 194
+            Polícia Militar: 190
+            Polícia Rodoviária Federal: 191
+            Procon-JP: 0800 83 2015
+            Samu: 192
+            Semob: 118
+            Sindicato dos Guias de Turismo da Paraíba: (83) 98855 9796 / 99946 7675
+            Vigilância Sanitária: (83) 3214-7956
+        </p>
+
+    <h2>Voltagem</h2>
+        <p>
+            A voltagem utilizada em João Pessoa é de 220V com tipo de tomada quadrifásicas.
+        </p>
+
+    <h2>Pontos de Informação Turística</h2>
+
+        <p> 
+            <strong>Prefeitura Municipal de João Pessoa</strong>
+            Secretaria de Turismo
+            Endereço: Rua Diogo Velho, 150, Centro, CEP: 58013-110
+            Telefone: +55 83 98161-3545
+        </p>
+        <p>
+            <strong>Centro de Atendimento ao Turista</strong>
+            Localização: Av. Almirante Tamandaré, 100 - Tambaú, CEP: 58.023-500
+            Telefone: +55 83 98654-7890
+            Horário de Funcionamento:
+            Segunda a sexta: 9h às 21h
+            Finais de semana e feriados: 9h às 19h
+            Centro de Apoio ao Turista Bosque dos Sonhos
+            Localização: Cabo Branco (Ao lado do Farol)
+            Horário de Funcionamento: Das 8h às 17h
+        </p>
+        
+        <p>
+            <strong>SINGTUR/PB - Sindicato dos Guias de Turismo da Paraíba</strong>
+            Endereço: A. Rui Carneiro, 241 - MAP, loja 113 -  Tambaú
+            Telefone: (83) 99600-1177
+        </p>
+
+    <h2>Segurança e Assistência</h2>
+        <p>
+            <strong>Delegacia de Atendimento ao Turista</strong>
+            Endereço: Centro Turístico de Tambaú - Av. Alm. Tamandaré, 100 - Tambaú
+            Telefones: (83) 3214 8022 / 3247 8405
+        </p>
+
+    <h2>Transportes</h2>
+        <p>
+            <strong>Aeroporto Castro Pinto</strong>
+            Endereço: Aeroporto, S/N, Bayeux-PB
+            Telefone: (83) 3041 4200
+        </p>
+        <p>
+            <strong>Rodoviária</strong>
+            Endereço: Rua Francisco Londres - Varadouro
+            Telefone: (83) 3222 6567
+        </p>
+    
+    <h2>Saúde</h2>
+        <p>
+            Hospital de Emergência e Trauma
+            Endereço: Av. Orestes Lisboa, S/N Conj. Pedro Gondim
+            Telefones: (83) 3216-5700
+        </p>
+        <p>
+            Hospital Valentina de Figueiredo
+            Endereço: Rua Mariângela Lucena Peixoto, 274 - Valentina
+            Telefone: (83) 3218-9404
+        </p>
+        <p>
+            Instituto Cândida Vargas
+            Endereço: Av. Coremas, S/N - Jaguaribe
+            Telefone: (83) 3015-1565
+        </p>
+        <p>
+            Complexo Hospitalar Governador Tarcísio Burity - Ortotrauma (Trauminha)
+            Endereço: Rua Agente Fiscal José Costa Duarte, S/N - Mangabeira
+            Telefone: (83) 3218-9725 | 3239-3225
+        </p>
+        <p>
+            UPA Oceania
+            Endereço: Av. Flávio Ribeiro Coutinho, 70 - Aeroclube
+            Telefone: (83) 3223-5209
+        </p>
+
+    <h2>Clima</h2>
+        <p>
+            João Pessoa possui um clima tropical, com temperaturas médias anuais em torno de 26°C.
+        </p>
+    
+    <h2>shoppings</h2>
+        <p>
+            <strong>Manaíra Shopping</strong>
+            Endereço: Avenida Governador Flávio Ribeiro Coutinho, 805 - Manaíra
+
+            <strong>Mag Shopping</strong>
+            Endereço: Avenida Governador Flávio Ribeiro Coutinho, 115 - Manaíra
+
+            <strong>Mangabeira Shopping</strong>
+            Endereço: Avenida Hilton Souto Maior, 1100 - Mangabeira
+
+            <strong>Shopping Sul</strong>
+            Endereço: Avenida Ayrton Senna da Silva, 3233 - Bairro dos Bancários
+
+            <strong>Shopping Tambiá</strong>
+            Endereço: Rua Almeida Barreto, 181 - Tambiá
+
+            <strong>Shopping Pátio Altiplano</strong>
+            Endereço: Avenida João Cirilo Silva, 155 - Altiplano Cabo Branco
+
+            <strong>Liv Mall</strong>
+            Endereço: Avenida Governador Flávio Ribeiro Coutinho, 500 - Manaíra
+
+            <strong>Parahyba Mall</strong>
+            R. Bacharel José de Oliveira Curchatuz, 542 - Jardim Oceania
+        </p>
+
+    <h2>Moeda e Câmbio</h2>
+        <p>
+            A moeda utilizada é o Real (BRL). Há casas de câmbio em diversos pontos da cidade, incluindo o aeroporto e shoppings.
+        </p>
+
+    <h2>Transporte Público</h2>
+        <p>
+            João Pessoa conta com um sistema de transporte público que inclui ônibus e táxis. Aplicativos de transporte como Uber e 99 também estão disponíveis.
+        </p>
+
+
+
+
+</body>
+</html>
